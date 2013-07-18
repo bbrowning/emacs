6be0be40c4a1c66f66443c167abdd2a8e514f6b7
Ben Browning <bbrownin@redhat.com>
2013-07-18 16:10:58 -0400

    ruby-mode write-file-hook is preventing .rb files from saving for some reason - disable for now

--- config/ruby.el
+++ config/ruby.el
@@ -19,10 +19,10 @@
 (setq auto-mode-alist (cons '("Gemfile" . ruby-mode) auto-mode-alist))
 (add-hook 'ruby-mode-hook
           (lambda ()
-            (add-hook 'local-write-file-hooks
-                      '(lambda()
-                         (save-excursion
-                           (untabify (point-min) (point-max)))))
+            ;(add-hook 'local-write-file-hooks
+            ;          '(lambda()
+            ;             (save-excursion
+            ;               (untabify (point-min) (point-max)))))
                            ;(delete-trailing-whitespace))))
             (set (make-local-variable 'indent-tabs-mode) 'nil)
             (set (make-local-variable 'tab-width) 2)
